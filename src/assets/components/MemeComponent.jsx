1c9247d38fb55e598c6cfad729cf01491acf884d
np03s190038 <thapa.sumit007@gmail.com>
2024-09-12 08:52:07 +0545

    Adding API call instead of local data.js

--- src/assets/components/MemeComponent.jsx
+++ src/assets/components/MemeComponent.jsx
@@ -1,38 +1,41 @@
-import memeData from "../../memeData";
+
 import React, {useState} from "react"
 
 export default function MemeComponent(){
-
     //This state holds the current values related to the meme being displayed or edited.
     const [meme, setMeme] = useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-
-    //This state holds the data for all available meme images, which can be used to randomly select a new meme image.
-    const [allMemeImages, setAllMemeImages] = useState(memeData)
     
+//This state holds the data for all available meme images, which can be displayed/rendered
+    const [allMemes, setAllMemes] = useState([])
+    
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
     
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
         }))
         
     }
-
-      function handleChange(event) {
+    
+    function handleChange(event) {
         const {name, value} = event.target
         setMeme(prevMeme => ({
             ...prevMeme,
             [name]: value
         }))
     }
-
+   
     return(
         <main>
             <div className="form">
